d8bbf1081c1ecacf6938adc14da6e61ab28eb52e
camilo.culm28@gmail.com <you@example.com>
2023-08-22 12:13:42 -0500

    se agregan botones para inicio y registro y se relacionan con su clase

---
+++ app/src/main/java/com/example/srtolima/Registrarse.java
@@ -0,0 +1,13 @@
+package com.example.srtolima;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Registrarse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse);
+    }
+
+}
--- app/src/main/java/com/example/srtolima/Splash.java
+++ app/src/main/java/com/example/srtolima/Splash.java
@@ -5,13 +5,14 @@ import android.media.MediaPlayer;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
 
     //Atributos
-    MediaPlayer cancion;
+    //MediaPlayer cancion;
 
     //Metodos
 
@@ -25,24 +26,28 @@ public class Splash extends AppCompatActivity {
         barra.hide();
 
         //Lanzar la cancion
-        cancion = MediaPlayer.create(this,R.raw.noche_buena);
-        cancion.start();
-
+        /*cancion = MediaPlayer.create(this,R.raw.noche_buena);
+        //cancion.start();
         //Temporizar la duracion del splash
         TimerTask inicioapp = new TimerTask() {
             @Override
             public void run() {
-
                 //Lanzar una nueva actividad
                 Intent intent = new Intent(Splash.this,MainActivity.class);
                 startActivity(intent);
                 cancion.stop();
-
             }
         };
-
         Timer tiempo = new Timer();
-        tiempo.schedule(inicioapp,10000);
+        tiempo.schedule(inicioapp,10000);*/
+    }
 
+    public void irIniciar(View view){
+        Intent i = new Intent(this, IniciarSesion.class);
+        startActivity(i);
+    }
+    public void irRegistrar(View view){
+        Intent i = new Intent(this, Registrarse.class);
+        startActivity(i);
     }
 }
