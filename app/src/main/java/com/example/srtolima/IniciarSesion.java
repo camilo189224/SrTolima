b88eeff5c029fab228db8288efadee1e6432854e
camilo.culm28@gmail.com <you@example.com>
2024-06-07 22:47:48 -0500

    se crean atributos y se instancia el boton de registro

--- app/src/main/java/com/example/srtolima/IniciarSesion.java
+++ app/src/main/java/com/example/srtolima/IniciarSesion.java
@@ -1,13 +1,47 @@
 package com.example.srtolima;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class IniciarSesion extends AppCompatActivity {
+
+    //ATRIBUTOS
+    EditText correo1, contrasena1;
+    Button btn_IniciarSesion1, btn_cerrarSesion1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
-    }
 
+        //asociar instanciar los botones
+        correo1 = findViewById(R.id.correoIniciar);
+        contrasena1 = findViewById(R.id.contrasenaIniciar);
+        btn_IniciarSesion1 = findViewById(R.id.btn_IniciarSesion);
+        btn_cerrarSesion1 = findViewById(R.id.btn_atrasini);
+
+        //evento click
+        btn_IniciarSesion1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String correoUser = correo1.getText().toString().trim();
+                String contrasenaUser = contrasena1.getText().toString().trim();
+
+                if (correoUser.isEmpty() && contrasenaUser.isEmpty()) {
+                    Toast.makeText(IniciarSesion.this, "Ingrese su correo y contraseña", Toast.LENGTH_SHORT).show();
+                }else{
+                    iniciarSesion(correoUser, contrasenaUser);
+                }
+            }
+        });
+    }
+    private void iniciarSesion(String correoUser, String contrasenaUser) {
+
+    }
 }
--- app/src/main/java/com/example/srtolima/Registrarse.java
+++ app/src/main/java/com/example/srtolima/Registrarse.java
@@ -1,13 +1,49 @@
 package com.example.srtolima;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registrarse extends AppCompatActivity {
+
+    EditText email, password, repassword;
+    Button btn_reg, btn_backreg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
+
+        email = findViewById(R.id.correo);
+        password = findViewById(R.id.password);
+        repassword = findViewById(R.id.contrasenaConf);
+        btn_reg = findViewById(R.id.btn_registro);
+        btn_backreg = findViewById(R.id.btn_atrasreg);
+
+        btn_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String emailUser = email.getText().toString().trim();
+                String passwordUser = password.getText().toString().trim();
+                String repasswordUser = repassword.getText().toString().trim();
+
+                if (emailUser.isEmpty() && passwordUser.isEmpty() && repasswordUser.isEmpty()){
+                    Toast.makeText(Registrarse.this, "complete los datos", Toast.LENGTH_SHORT).show();
+                }else {
+                    registerUser(emailUser, passwordUser, repasswordUser);
+                }
+            }
+        });
     }
 
+    private void registerUser(String emailUser, String passwordUser, String repasswordUser) {
+
+    }
 }
+
+
+
