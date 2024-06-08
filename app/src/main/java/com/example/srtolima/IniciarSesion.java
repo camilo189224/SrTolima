65166e1f906e6a4e7dbe51c9ffa72ac7baadaae6
camilo.culm28@gmail.com <you@example.com>
2024-06-07 21:10:49 -0500

    se crean atributos y se instancia el boton de registro

--- app/src/main/java/com/example/srtolima/IniciarSesion.java
+++ app/src/main/java/com/example/srtolima/IniciarSesion.java
@@ -1,26 +1,13 @@
 package com.example.srtolima;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class IniciarSesion extends AppCompatActivity {
-
-    //ATRIBUTOS
-    EditText correo1;
-    EditText contrasena1;
-    Button btn_IniciarSesion1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
-
-        //asociar instanciar los botones
-        correo1 = findViewById(R.id.correo);
-        contrasena1 = findViewById(R.id.password);
-        btn_IniciarSesion1 = findViewById(R.id.btn_IniciarSesion);
-
     }
 
 }
